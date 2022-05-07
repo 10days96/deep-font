3c565ba706b9d9110321e85c3993e9855035b9e6
10days96 <doradoralee@naver.com>
2022-05-07 18:00:43 +0900

    feat: menu바 UI 작성 완료

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,91 +1,96 @@
-import { useEffect, useState } from "react";
-import style from "components/Menu/Menu.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faFont } from "@fortawesome/free-solid-svg-icons";
+import { faFont, faPalette, faSquareCheck } from "@fortawesome/free-solid-svg-icons";
+import 'antd/dist/antd.css'
+import { Slider, InputNumber, Row, Col } from 'antd';
+import { useState } from "react";
+
+import style from "components/Menu/Menu.module.css";
+
+function HorizonLine(){
+    return (
+        <div className={style.horizonLine}
+        //   style={{
+        //     width: "90%",
+        //     textAlign: "center",
+        //     borderBottom: "1px solid #aaa",
+        //     lineHeight: "0.1em",
+        //     margin: "10px 0px 20px",
+        //   }}
+        >
+          {/* <span style={{ background: "#fff", padding: "0 10px" }}>{text}</span> */}
+          <span style={{ background: "#fff"}}></span>
+        </div>
+      ); 
+}
+
+function IntegerStep() {
+    
+
+    const [state, setState] = useState(1);
+    const inputValue = state;
+
+    const onChange = (value) => {
+        setState(value)
+    }
+
+    return(
+        <Row>
+            <Col span={12}>
+                <Slider
+                min={1}
+                max={20}
+                onChange={onChange}
+                value={typeof inputValue === 'number' ? inputValue : 0}
+            />
+            </Col>
+            <Col span={4}>
+                <InputNumber
+                    className={style.inputSlider}
+                    min={1}
+                    max={20}
+                    value={inputValue}
+                    onChange={onChange}
+                />
+            </Col>
+        </Row>
+    )
+
+}
 
 function Menu() {
-    const [selectedMenu, setSelectedMenu] = useState(0);
-
-    useEffect(() => {
-        setSelectedMenu(0);
-    }, []);
-
-    const onClickMenu = ({ target }) => {
-        target.id === selectedMenu
-            ? setSelectedMenu(0)
-            : setSelectedMenu(target.id);
-        console.log(target);
-        console.log(selectedMenu);
-    };
 
     return (
         <div className={style.menu}>
-            <div id={1} className={style.menu__item}>
-                <div className={style.menu__item__header}>
-                    <FontAwesomeIcon icon={faFont} />
-                    굵기
-                    {/* <span onClick={onClickMenu} className="" */}
-                    <FontAwesomeIcon icon={faAngleDown} onClick={onClickMenu} />
-                    <div></div>
-                </div>
-                <div
-                    className={
-                        selectedMenu === "1"
-                            ? [style.onFocus, style.menu__item__contents]
-                            : style.menu__item__contents
-                    }
-                >
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </div>
+            <div className={style.menu__item}>
+                <FontAwesomeIcon icon={faFont} className={style.icon_padding}/>
+                굵기
+                <HorizonLine/>
+                <IntegerStep/>
             </div>
-            <div id={2} className={style.menu__item}>
-                <div className={style.menu__item__header}>
-                    <FontAwesomeIcon icon={faFont} />
-                    스타일
-                    <FontAwesomeIcon icon={faAngleDown} onClick={onClickMenu} />
-                </div>
-                <div
-                    className={style.menu__item__contents}
-                    // className={selectedMenu === "2" ? style.onFocus : ""}
-                >
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </div>
-            </div>
-            <div id={3} className={style.menu__item}>
-                <div className={style.menu__item__header}>
-                    <FontAwesomeIcon icon={faFont} />
-                    검토
-                    <FontAwesomeIcon icon={faAngleDown} onClick={onClickMenu} />
-                </div>
-                <div
-                    className={style.menu__item__contents}
-                    // className={selectedMenu === "3" ? style.onFocus : ""}
-                >
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </div>
-            </div>
-            {/* <div id={2} onClick={onClickMenu}>
+            <div className={style.menu__item}>
+                <FontAwesomeIcon icon={faPalette} className={style.icon_padding}/>
                 스타일
-                <div className={selectedMenu === "2" ? style.onFocus : ""}>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
+                <HorizonLine />
+                <div className={style.font_style_grid}>
+                    <div className={style.font_style_button}>
+                        <button className={style.font_style_button_contrast}>
+                            별
+                        </button>
+                        대비
+                    </div>
+                    <div className={style.font_style_button}>
+                        <button className={style.font_style_button_round}>
+                            뚠
+                        </button>
+                        둥글게
+                    </div>
                 </div>
-            </div>
-            <div id={3} onClick={onClickMenu}>
+            </div >
+            <HorizonLine />
+            <div className={style.menu__item}>
+                <FontAwesomeIcon icon={faSquareCheck} className={style.icon_padding}/>
                 검토
-                <div className={selectedMenu === "3" ? style.onFocus : ""}>
-                    <li>7</li>
-                    <li>8</li>
-                    <li>9</li>
-                </div>
-            </div> */}
+            </div>
         </div>
     );
 }
